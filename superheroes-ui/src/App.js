57f31579012bbb89fc1ca6d59f64e48613b23bbc
= <=>
2023-07-31 16:57:17 +0100

    2nd commit

--- superheroes-ui/src/App.js
+++ superheroes-ui/src/App.js
@@ -1,11 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import AllHeroes from './components/AllHeroes';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom'
+import HeroForm from './components/HeroForm';
 
 function App() {
   return (
-    <AllHeroes/>
+    <>
+
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Heroes</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/allheroes">All Heroes</Nav.Link>
+              <Nav.Link href="/addhero">Add Hero</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="allheroes" element={<AllHeroes />} />
+        <Route path="addhero" element={<HeroForm />} />
+        <Route path="/" element={<AllHeroes />} />
+      </Routes>
+    </>
   );
+
 }
 
 export default App;
